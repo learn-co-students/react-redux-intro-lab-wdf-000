cc33d4e602a3d82c576590d3de822f42c86751ad
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-11-09 09:09:49 -0500

    container component test passing using redux-mock-store

---
+++ test/app-test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { shallow, mount} from 'enzyme'
+import expect, { createSpy, spyOn, isSpy } from 'expect'
+import ReactTestUtils from 'react-addons-test-utils'
+import App from '../src/App'
+import InventoryList from '../src/InventoryList'
+
+import configureStore from 'redux-mock-store';
+import { Provider, connect} from 'react-redux';
+
+const items = [{quantity: '3lbs', description: "flour"}, {quantity: "2 dozen", description: "eggs"}, {quantity: '2lbs', description: "sugar"}]
+
+function setup() {
+  const props = {
+    inventoryListItems: items
+  }
+  const mockStore = configureStore([]);
+  const store = mockStore({});
+  const wrapper = shallow(<App {...props} store={store}/>).shallow()
+
+  return {
+    props,
+    wrapper
+  }
+}
+
+describe('<App/>', function () {
+
+  it('should render the InventoryList component as a child', function () {
+    const { store, props, wrapper } = setup()
+    console.log(wrapper.instance())
+    expect(wrapper.find('InventoryList').length).toEqual(1)
+  });
+
+ 
+});
--- test/inventory-list-test.js
+++ test/inventory-list-test.js
@@ -1,15 +1,20 @@
-// const React = require('react');
-// const { mount, shallow } = require('enzyme');
-// const InventoryList = require('../src/InventoryList');
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import expect, { createSpy, spyOn, isSpy } from 'expect'
+import ReactTestUtils from 'react-addons-test-utils'
+import InventoryList from '../src/InventoryList'
 
-// describe('<InventoryList/>', function () {
-//   it('should display a list of inventory items with the item quantity and name', function () {
-//     const wrapper = shallow(<InventoryList/>);
-//     expect(wrapper.find('li')).to.have.length(3);
-//   });
+const items = [{quantity: '3lbs', description: "flour"}, {quantity: "2 dozen", description: "eggs"}, {quantity: '2lbs', description: "sugar"}]
 
-//   it('should have props inventoryItems', function () {
-//     const wrapper = shallow(<InventoryList/>);
-//     expect(wrapper.props().inventoryItems).to.be.defined;
-//   });
-// });
+describe('<InventoryList/>', function () {
+
+  it('should display a list of inventory items with the item quantity and name', function () {
+    const wrapper = mount(<InventoryList inventoryItems={items}/>);
+    expect(wrapper.find('li').length).toEqual(3);
+  });
+
+  it('should have props inventoryItems', function () {
+    const wrapper = shallow(<InventoryList inventoryItems={items}/>);
+    expect(wrapper.props().inventoryItems).toBe.defined;
+  });
+});
