a10cbba4ffe150ede264b9a521d08afd2cb3ba99
sami4thewin <samiomarmohammed@gmail.com>
2017-03-16 09:29:36 -0400

    Rough version

--- src/InventoryList.js
+++ src/InventoryList.js
@@ -3,8 +3,8 @@ import React from 'react';
 const InventoryList = (props) => {
   return (
     <ul className="list-group col-lg-6 col-lg-offset-3">
-      {props.items.map((item, i) => {
-        return <li key={i} className="list-group-item">{item.description}</li>
+      {props.inventoryItems.map((item, i) => {
+        return <li key={i} className="list-group-item">item: {item.description}, quantity: {item.quantity}</li>
       })}
     </ul>
   )
---
+++ src/NewInventoryItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const NewInventoryItem = (props) => {
+  function submitItemForm(event) {
+    event.preventDefault();
+    const quantity = event.target.children[1].value
+    const description = event.target.children[3].value
+    // props.triggerCreateItem(quantity, description)
+  }
+  return (
+    <div>
+    <form className="col-lg-6 col-lg-offset-3 form-horizontal" onSubmit={submitItemForm}>
+      <label>new item quantity:</label>
+      <input className="form-control"/>
+       <label>new item description:</label>
+      <input className="form-control"/>
+      <input type="submit" />
+    </form>
+    </div>
+  )
+}
+
+export default NewInventoryItem;
